f1be46e14a61443e6643d1a6fc0d689a935059e6
martinspenkevics <Martins.Penkevics@emergn.com>
2023-01-25 11:56:27 +0200

    Updated styling

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { defineCustomElement as defineButton } from "emergn-design-poc/dist/components/emergn-button";
 import { defineCustomElement as defineInput } from "emergn-design-poc/dist/components/emergn-input";
 
+import "emergn-design-poc/dist/emergn-design-poc/emergn-design-poc.css";
 import "./custom-theme.css";
 
 defineButton();
