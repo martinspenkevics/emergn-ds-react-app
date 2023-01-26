7901a48b2c4f03b74550a1cc81e964b6d1f14cbd
martinspenkevics <Martins.Penkevics@emergn.com>
2023-01-26 11:53:58 +0200

    React components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
 import { useState } from "react";
+import { EmergnButton, EmergnInput } from "emergn-design-poc-react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <emergn-button
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "1rem",
+          marginBottom: "1rem",
+        }}
+      >
+        <EmergnButton
           label={"Click Me " + count}
           onClick={() => setCount(count + 1)}
         />
-        <emergn-button
+        <EmergnButton
           variant="secondary"
           label={"Click Me " + count}
           onClick={() => setCount(count + 1)}
         />
-        <emergn-button
+        <EmergnButton
           variant="tertiary"
           label={"Click Me " + count}
           onClick={() => setCount(count + 1)}
         />
       </div>
 
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <emergn-input
-          id="text1"
-          size="sm"
-          onInput={() => setCount(count + 1)}
-        />
-        <emergn-input id="text2" onInput={() => setCount(count + 1)} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "1rem",
+          marginBottom: "1rem",
+        }}
+      >
+        <EmergnInput size="sm" onInput={() => setCount(count + 1)} />
+        <EmergnInput onInput={() => setCount(count + 1)} />
       </div>
     </div>
   );
